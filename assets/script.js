04d319cd2e14f14280b7f612d7ed05c316a9162b
Jerwin <jerwinsauco@gmail.com>
2022-07-22 11:11:27 -0400

    commit file merge

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+
+// We start with a button labelled "start" prompting user to start the quiz
+document.getElementById("btn").addEventListener("click", function(){
+    //* timer stats after pressing btn
+    // insert for loop here + 
+    // setTimeout to incremenet/decrement 
+    document.getElementById("questionText").textContent = "Question1"
+    
+})
+
+
+// display the questions, the first question!!!!
+// use var for questions or array or object
+// then display on the page
+
+var answer
+
+
+
+//Starts a timer to track how long an operation takes. 
+console.time()
+
+// setTimeout is to provide elapse time value
+setTimeout()
